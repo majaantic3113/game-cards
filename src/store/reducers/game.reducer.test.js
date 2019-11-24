02877860355415d564d46d6a09d6547b2ff7c7bd
Marija Antic <m.antic@levi9.com>
2019-11-24 23:29:43 +0100

    test for game reducer done

--- src/store/reducers/game.reducer.test.js
+++ src/store/reducers/game.reducer.test.js
@@ -5,6 +5,10 @@ import {
   FETCH_CARDS_SUCCESS,
   FETCH_CARDS_ERROR,
   THROW_CARD_COMPUTER,
+  THROW_CARD_PLAYER,
+  FIND_ROUND_WINNER,
+  CALCULATE_SCORE,
+  CLEANUP_BOARD,
 } from '../actions/game.actions';
 
 const stateExample = {
@@ -22,6 +26,7 @@ const stateExample = {
 jest.mock('../../config', () => {
   return {
     NUMBER_OF_CARDS_PER_PLAYER: 2,
+    CARD_VALUES: { 8: 8 },
   };
 });
 
@@ -118,6 +123,147 @@ describe('game reducer test', () => {
         player2: { cards: [], score: 0 },
       },
       moveInProgress: true,
+    });
+  });
+
+  it('should return updated state after throwCArdPlayer', () => {
+    const currentState = {
+      ...stateExample,
+      players: {
+        player1: {
+          cards: [{ code: '8', image: 'sth', value: '8' }],
+          score: 0,
+        },
+        player2: {
+          cards: [{ code: '10', image: 'sth', value: '10' }],
+          score: 0,
+        },
+      },
+    };
+
+    expect(
+      gameReducer(currentState, {
+        type: THROW_CARD_PLAYER,
+        payload: {
+          user: 'player1',
+          card: { code: '8', image: 'sth', value: '8' },
+        },
+      }),
+    ).toEqual({
+      ...currentState,
+      cardsOnTable: [
+        { user: 'player1', card: { code: '8', image: 'sth', value: '8' } },
+      ],
+      players: {
+        ...currentState.players,
+        player1: { cards: [], score: 0 },
+      },
+      moveInProgress: true,
+    });
+  });
+
+  it('should return updated state after findRoundWinner', () => {
+    const currentState = {
+      ...stateExample,
+      cardsOnTable: [
+        { user: 'player1', card: { code: '8', image: 'sth', value: '8' } },
+        { user: 'player2', card: { code: '8', image: 'sth', value: '8' } },
+      ],
+    };
+
+    jest.mock('../../util/util', () => {
+      return {
+        findGreatestCard: () => ({
+          user: 'player2',
+          card: { code: '8', image: 'sth', value: '8' },
+        }),
+      };
+    });
+
+    expect(
+      gameReducer(currentState, {
+        type: FIND_ROUND_WINNER,
+      }),
+    ).toEqual({
+      ...currentState,
+      roundWinnerCard: {
+        user: 'player2',
+        card: { code: '8', image: 'sth', value: '8' },
+      },
+    });
+  });
+
+  it('should return updated state after calculateScore', () => {
+    const currentState = {
+      ...stateExample,
+      cardsOnTable: [
+        { user: 'player1', card: { code: '8', image: 'sth', value: '8' } },
+        { user: 'player2', card: { code: '8', image: 'sth', value: '8' } },
+      ],
+      players: {
+        player1: {
+          cards: [],
+          score: 0,
+        },
+        player2: {
+          cards: [],
+          score: 10,
+        },
+      },
+      roundWinnerCard: {
+        user: 'player2',
+        card: { code: '8', image: 'sth', value: '8' },
+      },
+    };
+
+    expect(
+      gameReducer(currentState, {
+        type: CALCULATE_SCORE,
+      }),
+    ).toEqual({
+      ...currentState,
+      players: {
+        ...currentState.players,
+        player2: { cards: [], score: 26 },
+      },
+    });
+  });
+
+  it('should return updated state after cleanupBoard', () => {
+    const currentState = {
+      ...stateExample,
+      players: {
+        player1: {
+          cards: [],
+          score: 0,
+        },
+        player2: {
+          cards: [],
+          score: 10,
+        },
+      },
+      roundWinnerCard: {
+        user: 'player2',
+        card: { code: '8', image: 'sth', value: '8' },
+      },
+    };
+
+    jest.mock('../../util/util', () => {
+      return {
+        getWinner: () => 'player2',
+      };
+    });
+
+    expect(
+      gameReducer(currentState, {
+        type: CLEANUP_BOARD,
+      }),
+    ).toEqual({
+      ...currentState,
+      roundWinnerCard: null,
+      cardsOnTable: [],
+      moveInProgress: false,
+      gameWinner: 'player2',
     });
   });
 });
