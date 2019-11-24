c5d31189011cdd29ecbfdf196448843a9290f8a3
Marija Antic <m.antic@levi9.com>
2019-11-24 10:18:53 +0100

    eslint setup

--- src/App.test.js
+++ src/App.test.js
@@ -1,35 +1,35 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import { App, mapStateToProps } from "./App";
-import { Provider } from "react-redux";
-import store from "./store";
+import React from 'react';
+import { render } from '@testing-library/react';
+import { App, mapStateToProps } from './App';
+import { Provider } from 'react-redux';
+import store from './store';
 
-describe("Test App component", () => {
-  it("should render correctly", () => {
+describe('Test App component', () => {
+  it('should render correctly', () => {
     const { container } = render(
       <Provider store={store}>
         <App />
-      </Provider>
+      </Provider>,
     );
 
     expect(container.firstChild).toMatchSnapshot();
   });
 
-  it("should render correctly", () => {
+  it('should render correctly', () => {
     const { container } = render(
       <Provider store={store}>
         <App numberOfPlayers={2} />
-      </Provider>
+      </Provider>,
     );
 
     expect(container.firstChild).toMatchSnapshot();
   });
 });
 
-describe("mapStateToProps", () => {
-  it("should provide number of players prop", () => {
+describe('mapStateToProps', () => {
+  it('should provide number of players prop', () => {
     const state = { game: { numberOfPlayers: 2 } };
 
-    expect(mapStateToProps(state)).toHaveProperty("numberOfPlayers");
+    expect(mapStateToProps(state)).toHaveProperty('numberOfPlayers');
   });
 });
